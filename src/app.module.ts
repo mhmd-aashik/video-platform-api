e9342b87c669d3574e8d3ce10f129252127296aa
Mohammed Aashik <aashikdevelop@gmail.com>
2026-09-19 12:04:48 +0400

    Add class-transformer and class-validator dependencies; integrate OrganizationsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, OrganizationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
