49b6a3fdad36937917ce2c6e99fd76a04b32cf48
Mohammed Aashik <aashikdevelop@gmail.com>
2026-09-19 12:23:03 +0400

    Add JWT authentication support and user retrieval endpoint
    - Introduced JwtAuthGuard for protecting routes with JWT authentication.
    - Added a new 'getMe' endpoint in AuthController to retrieve the current user's information.
    - Updated AuthModule to include PassportModule and JwtStrategy for enhanced authentication capabilities.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       global: true,
       secret: process.env.JWT_ACCESS_SECRET,
@@ -14,6 +17,6 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
